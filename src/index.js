16d24811c0c72f9bd503c226615320325588be4a
Daniel Ochuba <danielochuba78@gmail.com>
2023-05-24 00:45:25 +0100

    Make review changes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import './style.css';
 import addScore from './modules/submitScore.js';
 import displayScores from './modules/displayScore.js';
 
-// const submitBtn = document.querySelector('.submit-btn');
 const refreshBtn = document.querySelector('.refresh-btn');
 const form = document.querySelector('.form-input');
+
+document.addEventListener('DOMContentLoaded', () => {
+  displayScores();
+});
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
